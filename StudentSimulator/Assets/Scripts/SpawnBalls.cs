1e78c37033458596b44c8a5cc75ef656ade73b4b
someinternetuser <aleksandra.czerniawska@student.put.poznan.pl>
2018-04-21 16:35:23 +0200

    Added ball spawn
    Co-Authored-By: Qitay <klempka.m@gmail.com>

---
+++ StudentSimulator/Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour
+{
+    // Use this for initialization
+    private Vector3 startpos;
+    private Quaternion startrot;
+    public GameObject ball;
+    public GameObject spawningPoint;
+
+    void Start()
+    {
+        SpawnCrumple();
+        //Debug.Log(GetComponent<Rigidbody>().name);
+    }
+
+   
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log(collider);
+        if (collider.CompareTag("Crumple"))
+        {
+            //Debug.Log(GetComponent<Rigidbody>().name);
+            SpawnCrumple();
+        }
+        //GameController.Instance.Punish();
+        /*if(collision.gameObject.name == "ostry" && przypal) {
+            GameController::Instance().Punish();
+        }*/
+    }
+
+    private void SpawnCrumple()
+    {
+        Instantiate(ball, spawningPoint.transform.position, spawningPoint.transform.rotation);
+
+    }
+
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
