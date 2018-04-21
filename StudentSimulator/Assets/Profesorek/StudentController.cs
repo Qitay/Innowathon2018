fc1f2f2fee4c8de747557e4cbfb768873b42718e
MatthewRock <maliszmat@gmail.com>
2018-04-21 05:11:24 +0200

    Profesorek stub

---
+++ StudentSimulator/Assets/Profesorek/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StudentController : MonoBehaviour {
+
+    //TODO: Set to true when not holding pen/paper anchor, false otherwise.
+    public bool przypal;
+    public int lives;
+
+	void Start () {
+      przypal = false;
+      lives = 3;
+	}
+
+	void Update () {
+      //Quaternion.Euler();
+
+      //Vector3.Lerp(mojVector, vectorDoKtoregoDazysz, zmiennaDoVectora?? * Time.DeltaTime );
+	}
+
+    void OnTriggerEnter(Collider collider) {
+        Debug.Log(collider.gameObject.name);
+        GameController.Instance.Punish();
+        /*if(collision.gameObject.name == "ostry" && przypal) {
+            GameController::Instance().Punish();
+        }*/
+    }
+}
