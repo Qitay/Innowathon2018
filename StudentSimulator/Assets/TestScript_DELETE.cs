f80a6a2c9d71f88ea14639435290a706bcf912f9
someinternetuser <aleksandra.czerniawska@student.put.poznan.pl>
2018-04-21 12:19:28 +0200

    Throwing balls
    Co-Authored-By: Qitay <klempka.m@gmail.com>

---
+++ StudentSimulator/Assets/TestScript_DELETE.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript_DELETE : MonoBehaviour {
+
+    private GameObject Controller;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+    }
+}
