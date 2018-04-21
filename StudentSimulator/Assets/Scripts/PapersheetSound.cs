acb67683133090298682ac569d9bbb4fcf86eed2
Serq0 <sergiuszjanczura@gmail.com>
2018-04-21 11:21:20 +0200

    Papersheet writing script

---
+++ StudentSimulator/Assets/Scripts/PapersheetSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PapersheetSound : MonoBehaviour {
+
+    public AudioClip[] writingAudio;
+    AudioSource audio;
+    Transform transform;
+
+    // Use this for initialization
+    void Start () {
+        //transform.localScale = new Vector3(0.01379639f, 0.05f, 0.01840163f);
+        audio = GetComponent<AudioSource>();
+        audio.volume = 0.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //AudioSource.PlayClipAtPoint(writingAudio[0], transform.position, 1.5f);
+        if(audio.isPlaying == false)
+        {
+            audio.clip = writingAudio[Random.Range(0,writingAudio.Length)];
+            audio.Play();
+        }
+	}
+}
